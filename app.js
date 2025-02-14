c422f152f5d00e17129f21f872b6e3e08af60bd3
Isaksend <islamkadyrbekov31@gmail.com>
2025-02-14 16:02:08 +0500

    added url of frontend

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://front-isa-blog.vercel.app/",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
