33c92efbb714b3aa7a778738b8e5f9fae507037e
Isaksend <islamkadyrbekov31@gmail.com>
2025-02-14 14:16:06 +0500

    Фикс CORS и SameSite для куков

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const app = express();
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -35,13 +35,20 @@ app.use(
             collectionName: 'sessions',
         }),
         cookie: {
-            httpOnly: true,
-            secure: true,
-            sameSite: 'None',
-            maxAge: 1000 * 60 * 60 * 24,
+            httpOnly: true, // Куки не доступны JavaScript на клиенте
+            secure: true, // Обязательно true для HTTPS
+            sameSite: 'None', // Разрешает передачу кук между разными доменами
+            maxAge: 1000 * 60 * 60 * 24, // 1 день
         },
     })
 );
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
@@ -52,12 +59,6 @@ app.use((req, res, next) => {
     console.log("ЗАГОЛОВКИ НА СЕРВЕРЕ:", req.headers);
     next();
 });
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
-
-
 
 // Подключение Passport
 app.use(passport.initialize());
